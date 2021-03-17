1d64abd9feb48a88e41567914d6505b9acafb2c4
CptSpooky <legacyofthedonut@gmail.com>
2021-03-17 16:09:26 -0400

    commented out contact for now

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -20,9 +20,9 @@ const Header = ({ currentUser, hidden }) => (
       <Link className="option" to="/shop">
         SHOP
       </Link>
-      <Link className="option" to="/shop">
-        CONTACT
-      </Link>
+     {/* <Link className="option" to="/shop">
+            CONTACT
+         </Link> */}
       {
         currentUser ? 
         <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
